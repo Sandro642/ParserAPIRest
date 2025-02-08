a78a61eb493afe0ec79ab46019816e4b2c01e425
Sandro SOria <sandro33810@gmail.com>
2025-02-08 14:29:19 +0100

    Add UseSetRoutes class and implement route configuration; enhance JobRoutes with route properties and methods

--- src/main/java/fr/sandro642/github/jobs/JobRoutes.java
+++ src/main/java/fr/sandro642/github/jobs/JobRoutes.java
@@ -1,5 +1,39 @@
 package fr.sandro642.github.jobs;
 
+import fr.sandro642.github.main.routes.setter.SetRoutes;
+
 public abstract class JobRoutes {
 
+    protected String routeName;
+    protected String routeBranch;
+    protected String routeUrl;
+    protected String routeMethod;
+    protected String routeDescription;
+
+    public abstract void setRouteName(String routeName);
+    public abstract void setRouteBranch(String routeBranch);
+    public abstract void setRouteUrl(String routeUrl);
+    public abstract void setRouteMethod(String routeMethod);
+    public abstract void setRouteDescription(String routeDescription);
+    public abstract void SaveRoutes(SetRoutes setRoutes);
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public String getRouteBranch() {
+        return routeBranch;
+    }
+
+    public String getRouteUrl() {
+        return routeUrl;
+    }
+
+    public String getRouteMethod() {
+        return routeMethod;
+    }
+
+    public String getRouteDescription() {
+        return routeDescription;
+    }
 }
