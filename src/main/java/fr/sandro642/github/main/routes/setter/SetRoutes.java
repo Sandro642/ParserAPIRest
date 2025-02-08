c8d743215951d86bda25a9077660b8c4ea648a09
Sandro SOria <sandro33810@gmail.com>
2025-02-08 11:52:55 +0100

    Rename JobRoutes package and update imports in GetRoutes and SetRoutes

--- src/main/java/fr/sandro642/github/main/routes/setter/SetRoutes.java
+++ src/main/java/fr/sandro642/github/main/routes/setter/SetRoutes.java
@@ -1,6 +1,6 @@
 package fr.sandro642.github.main.routes.setter;
 
-import fr.sandro642.github.api.jobs.JobRoutes;
+import fr.sandro642.github.jobs.JobRoutes;
 
 public class SetRoutes extends JobRoutes {
 
